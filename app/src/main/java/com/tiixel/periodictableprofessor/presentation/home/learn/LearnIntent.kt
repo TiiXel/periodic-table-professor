3381d721f076f0572456ba8bda30a7d446bfb0f5
Rémi Berthoz <remi.berthoz@gmail.com>
2018-07-10 10:32:53 +0200

    Remove unused package in presentation layer

--- app/src/main/java/com/tiixel/periodictableprofessor/presentation/home/learn/LearnIntent.kt
+++
@@ -1,8 +0,0 @@
-package com.tiixel.periodictableprofessor.presentation.home.learn
-
-import com.tiixel.periodictableprofessor.presentation.base.MviIntent
-
-sealed class LearnIntent  : MviIntent {
-
-    object InitialIntent : LearnIntent()
-}
